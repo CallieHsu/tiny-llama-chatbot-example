837baafaa78ab2de4acdf29452e4fed63786274e
CallieHsu <kelly811113@gmail.com>
2024-03-05 13:32:09 +0800

    first commit

---
+++ gradio_chatbot_tiny-llama-shuttle_demo.py
@@ -0,0 +1,252 @@
+from transformers import AutoTokenizer, TextIteratorStreamer
+from optimum.intel.openvino import OVModelForCausalLM
+from time import perf_counter
+from openvino.runtime import Core
+import argparse
+from threading import Thread
+from pathlib import Path
+import gradio as gr
+from typing import List
+import numpy as np
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('-h',
+                    '--help',
+                    action='help',
+                    help='Show this help message and exit.')
+parser.add_argument('-m',
+                    '--model_id',
+                    required=True,
+                    type=str,
+                    help='Required. openvino model path')
+parser.add_argument('-d',
+                    '--device',
+                    default='CPU',
+                    required=False,
+                    type=str,
+                    help='device for inference')
+args = parser.parse_args()
+
+core = Core()
+available_devices = core.available_devices
+current_device = "CPU"
+
+print(" --- load tokenizer --- ")
+tokenizer = AutoTokenizer.from_pretrained(args.model_id)
+
+try:
+    print(" --- use local model --- ")
+    model = OVModelForCausalLM.from_pretrained(args.model_id, compile=False, device=args.device)
+except:
+    print(" --- use remote model --- ")
+    model = OVModelForCausalLM.from_pretrained(args.model_id, compile=False, device=args.device, export=True)
+model.compile()
+
+INTRO_BLURB = (
+    "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+)
+
+## Tiny-Llama prompt format
+PROMPT_FOR_GENERATION_FORMAT = """<|system|>
+{intro}</s>
+<|user|>
+{instruction}</s>
+<|assistant|>
+""".format(
+    intro=INTRO_BLURB,
+    instruction="{instruction}"
+)
+
+# def run_generation(user_text: str, top_p: float, temperature: float,
+#                    top_k: int, max_new_tokens: int, perf_text: str):
+def run_generation(user_text: str, max_new_tokens: int, perf_text: str):
+    # Prepare input prompt according to model expected template
+    prompt_text = PROMPT_FOR_GENERATION_FORMAT.format(instruction=user_text)
+
+    # Tokenize the user text.
+    model_inputs = tokenizer(prompt_text, return_tensors="pt")
+
+    # Start generation on a separate thread, so that we don't block the UI. The text is pulled from the streamer
+    # in the main thread. Adds timeout to the streamer to handle exceptions in the generation thread.
+    streamer = TextIteratorStreamer(tokenizer,
+                                    skip_prompt=True,
+                                    skip_special_tokens=True)
+    generate_kwargs = dict(model_inputs,
+                           streamer=streamer,
+                           max_new_tokens=max_new_tokens,
+                        #    do_sample=True,
+                        #    top_p=top_p,
+                        #    temperature=float(temperature),
+                        #    top_k=top_k,
+                           eos_token_id=tokenizer.eos_token_id,
+                           pad_token_id=tokenizer.eos_token_id)
+    t = Thread(target=model.generate, kwargs=generate_kwargs)
+    t.start()
+
+    # Pull the generated text from the streamer, and update the model output.
+    model_output = ""
+    per_token_time = []
+    num_tokens = 0
+    start = perf_counter()
+    for new_text in streamer:
+        current_time = perf_counter() - start
+        model_output += new_text
+        perf_text, num_tokens = estimate_latency(current_time, perf_text,
+                                                 new_text, per_token_time,
+                                                 num_tokens)
+        yield model_output, perf_text
+        start = perf_counter()
+    return model_output, perf_text
+
+
+def estimate_latency(current_time: float, current_perf_text: str,
+                     new_gen_text: str, per_token_time: List[float],
+                     num_tokens: int):
+    """
+    Helper function for performance estimation
+
+    Parameters:
+      current_time (float): This step time in seconds.
+      current_perf_text (str): Current content of performance UI field.
+      new_gen_text (str): New generated text.
+      per_token_time (List[float]): history of performance from previous steps.
+      num_tokens (int): Total number of generated tokens.
+
+    Returns:
+      update for performance text field
+      update for a total number of tokens
+    """
+    num_current_toks = len(tokenizer.encode(new_gen_text))
+    num_tokens += num_current_toks
+    per_token_time.append(num_current_toks / current_time)
+    if len(per_token_time) > 10 and len(per_token_time) % 4 == 0:
+        current_bucket = per_token_time[:-10]
+        return f"Average generaton speed: {np.mean(current_bucket):.2f} tokens/s. Total generated tokens: {num_tokens}", num_tokens
+    return current_perf_text, num_tokens
+
+
+def reset_textbox(instruction: str, response: str, perf: str):
+    """
+    Helper function for resetting content of all text fields
+
+    Parameters:
+      instruction (str): Content of user instruction field.
+      response (str): Content of model response field.
+      perf (str): Content of performance info filed
+
+    Returns:
+      empty string for each placeholder
+    """
+    return "", "", ""
+
+
+def select_device(device_str: str,
+                  current_text: str = "",
+                  progress: gr.Progress = gr.Progress()):
+    """
+    Helper function for uploading model on the device.
+
+    Parameters:
+      device_str (str): Device name.
+      current_text (str): Current content of user instruction field (used only for backup purposes, temporally replacing it on the progress bar during model loading).
+      progress (gr.Progress): gradio progress tracker
+    Returns:
+      current_text
+    """
+    if device_str != model._device:
+        model.request = None
+        model._device = device_str
+
+        for i in progress.tqdm(range(1),
+                               desc=f"Model loading on {device_str}"):
+            model.compile()
+    return current_text
+
+
+examples = [
+    "What is the processor support for the Shuttle XPC cube SH610R4?",
+    "Which generation of Intel processors does the Shuttle XPC cube SW580R8 support?",
+    "What are the CPU choices for the Shuttle XPC cube SH510R4?",
+    "Tell me about the chipset used in the Shuttle XPC cube SH570R8.",
+    "What are the dimensions of the Shuttle XPC cube SZ270R9?",
+    "What is the maximum power output of the power supply in the Shuttle XPC cube SH310R4 V2?",
+    "Please provide me the comparison of CPU specifications of Shuttle xpc cube series."
+]
+
+with gr.Blocks() as demo:
+    gr.Markdown(
+        "# Instruction following using Tiny-Llama-shuttle-xpc-cube and OpenVINO.\n"
+        "Provide insturction which describes a task below or select among predefined examples and model writes response that performs requested task.\n\n"
+        f"Model: {args.model_id}"
+    )
+
+    with gr.Row():
+        with gr.Column(scale=4):
+            user_text = gr.Textbox(
+                placeholder="Write an email about an alpaca that likes flan",
+                label="User instruction")
+            model_output = gr.Textbox(label="Model response",
+                                      interactive=False)
+            performance = gr.Textbox(label="Performance",
+                                     lines=1,
+                                     interactive=False)
+            with gr.Column(scale=1):
+                button_submit = gr.Button(value="Submit")
+                # button_stop = gr.Button(value="Stop")
+                button_clear = gr.Button(value="Clear")
+            gr.Examples(examples, user_text)
+        with gr.Column(scale=1):
+            device = gr.Dropdown(choices=available_devices,
+                                 value=current_device,
+                                 label="Device")
+            max_new_tokens = gr.Slider(
+                minimum=1,
+                maximum=512,
+                value=128,
+                step=1,
+                interactive=True,
+                label="Max New Tokens",
+            )
+            # top_p = gr.Slider(
+            #     minimum=0.05,
+            #     maximum=1.0,
+            #     value=0.92,
+            #     step=0.05,
+            #     interactive=True,
+            #     label="Top-p (nucleus sampling)",
+            # )
+            # top_k = gr.Slider(
+            #     minimum=0,
+            #     maximum=50,
+            #     value=0,
+            #     step=1,
+            #     interactive=True,
+            #     label="Top-k",
+            # )
+            # temperature = gr.Slider(
+            #     minimum=0.1,
+            #     maximum=5.0,
+            #     value=0.8,
+            #     step=0.1,
+            #     interactive=True,
+            #     label="Temperature",
+            # )
+
+    user_text.submit(
+        run_generation,
+        [user_text, max_new_tokens, performance], # [user_text, top_p, temperature, top_k, max_new_tokens, performance],
+        [model_output, performance])
+    button_submit.click(select_device, [device, user_text], [user_text])
+    button_submit.click(
+        run_generation,
+        [user_text, max_new_tokens, performance], # [user_text, top_p, temperature, top_k, max_new_tokens, performance],
+        [model_output, performance])
+    # button_stop.click(fn=None, inputs=None, outputs=None, cancels=[button_submit])
+    button_clear.click(reset_textbox, [user_text, model_output, performance],
+                       [user_text, model_output, performance])
+    device.change(select_device, [device, user_text], [user_text])
+
+try:
+    demo.launch(share=True, height=800) # enable_queue=True, 
+except Exception:
+    demo.launch(share=True, height=800) # enable_queue=True, 
---
+++ openvino_IR_chat_dialogue_record.py
@@ -0,0 +1,210 @@
+from transformers import AutoConfig, AutoTokenizer, TextStreamer
+from optimum.intel import OVQuantizer
+from optimum.intel.openvino import OVModelForCausalLM
+import openvino as ov
+from pathlib import Path
+import shutil
+import nncf
+import gc
+import sys
+import time
+import argparse
+
+############################
+## Read your private model
+############################
+# from huggingface_hub import login
+# access_token_read = "Your_huggingface_token"
+# login(token = access_token_read)
+
+parser = argparse.ArgumentParser(add_help=True)
+parser.add_argument('-m',
+                    '--model_id',
+                    default="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+                    required=False,
+                    type=str,
+                    help='Huggingface model name')
+parser.add_argument('-o',
+                    '--output',
+                    default='compressed_model',
+                    required=False,
+                    type=str,
+                    help='Path to save the Openvino IR model')
+parser.add_argument('-p',
+                    '--precision',
+                    required=False,
+                    default="INT8",
+                    type=str,
+                    choices=["FP16", "INT8", "INT4"],
+                    help='FP16, INT8 or INT4')
+parser.add_argument('-d',
+                    '--device',
+                    required=False,
+                    default="CPU",
+                    type=str,
+                    choices=["CPU", "GPU"],
+                    help='CPU or GPU')
+args = parser.parse_args()
+
+def convert_to_fp16():
+    if (fp16_model_dir / "openvino_model.xml").exists():
+        return
+    ov_model = OVModelForCausalLM.from_pretrained(
+        model_id, export=True
+    )
+    ov_model.half()
+    ov_model.save_pretrained(fp16_model_dir)
+    del ov_model
+    gc.collect()
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.save_pretrained(fp16_model_dir)
+
+def convert_to_int8():
+    if (int8_model_dir / "openvino_model.xml").exists():
+        return
+    int8_model_dir.mkdir(parents=True, exist_ok=True)
+    if fp16_model_dir.exists():
+        ov_model = OVModelForCausalLM.from_pretrained(fp16_model_dir, compile=False)
+    else:
+        ov_model = OVModelForCausalLM.from_pretrained(
+            model_id, export=True, compile=False
+        )
+        ov_model.half()
+    quantizer = OVQuantizer.from_pretrained(ov_model)
+    quantizer.quantize(save_directory=int8_model_dir, weights_only=True)
+    del quantizer
+    del ov_model
+    gc.collect()
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.save_pretrained(int8_model_dir)
+
+def convert_to_int4():
+    model_compression_params = {
+        "mode": nncf.CompressWeightsMode.INT4_ASYM,
+        "group_size": 128,
+        "ratio": 0.8,
+    }
+    if (int4_model_dir / "openvino_model.xml").exists():
+        return
+    int4_model_dir.mkdir(parents=True, exist_ok=True)
+    if not fp16_model_dir.exists():
+        model = OVModelForCausalLM.from_pretrained(
+            model_id, export=True, compile=False
+        ).half()
+        model.config.save_pretrained(int4_model_dir)
+        ov_model = model._original_model
+        del model
+        gc.collect()
+    else:
+        ov_model = ov.Core().read_model(fp16_model_dir / "openvino_model.xml")
+        shutil.copy(fp16_model_dir / "config.json", int4_model_dir / "config.json")
+
+    compressed_model = nncf.compress_weights(ov_model, **model_compression_params)
+    ov.save_model(compressed_model, int4_model_dir / "openvino_model.xml")
+    del ov_model
+    del compressed_model
+    gc.collect()
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.save_pretrained(int4_model_dir)
+
+model_id = args.model_id
+output_dir = args.output
+
+fp16_model_dir = Path(output_dir) / "FP16"
+int8_model_dir = Path(output_dir) / "INT8"
+int4_model_dir = Path(output_dir) / "INT4"
+
+convert_to_fp16()
+convert_to_int8()
+convert_to_int4()
+
+model_to_run = args.precision
+device = args.device
+
+if model_to_run == "INT4":
+    model_dir = int4_model_dir
+elif model_to_run == "INT8":
+    model_dir = int8_model_dir
+else:
+    model_dir = fp16_model_dir
+print(f"Loading model from {model_dir}")
+
+try:
+    tokenizer = AutoTokenizer.from_pretrained(model_dir)
+except:
+    tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+    tokenizer.save_pretrained(model_dir)
+
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+
+ov_config = {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1", "CACHE_DIR": ""}  # To disable model caching, set CACHE_DIR to an empty string.
+try:
+    print(" --- use local model --- ")
+    model = OVModelForCausalLM.from_pretrained(
+        model_dir,
+        device=device,
+        ov_config=ov_config,
+        config=AutoConfig.from_pretrained(model_dir),
+    )
+except:
+    print(" --- use remote model --- ")
+    model = OVModelForCausalLM.from_pretrained(
+        model_dir,
+        device=device,
+        ov_config=ov_config,
+        config=AutoConfig.from_pretrained(model_dir),
+        export=True
+    )
+
+dialogue_history = [
+    {"role": "system", "content": "Below is an instruction that describes a task. Write a response that appropriately completes the request.",},
+]
+
+max_new_tokens = 254
+temperature, top_p, top_k, repetition_penalty = 0.1, 1, 50, 1.1
+streamer = TextStreamer(tokenizer,
+                        timeout=30.0,
+                        skip_prompt=True,
+                        skip_special_tokens=True)
+generate_kwargs = dict(
+    max_new_tokens=max_new_tokens,
+    pad_token_id=tokenizer.pad_token_id,
+    temperature=temperature,
+    do_sample=temperature > 0.0,
+    top_p=top_p,
+    top_k=top_k,
+    repetition_penalty=repetition_penalty,
+    streamer=streamer,
+)
+
+def truncate(dialogue_history, limit=400):
+    total = 0
+    truncate_dialogue = list()
+    for msg in reversed(dialogue_history[1:]):
+        total += len(tokenizer(msg["content"], return_tensors="pt").input_ids[0])
+        if total > limit:
+            break
+        truncate_dialogue.insert(0, msg)
+    truncate_dialogue.insert(0, dialogue_history[0])
+    return truncate_dialogue
+
+def chat(dialogue_history):
+    input_text = tokenizer.apply_chat_template(dialogue_history, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+    start = time.perf_counter()
+    response = model.generate(input_ids=input_text, **generate_kwargs)
+    end = time.perf_counter()
+    new_tokens = len(response[0]) - len(input_text[0])
+    print(f"[info] Generation of {new_tokens} tokens took {end - start:.3f} s on {device} ({(new_tokens / (end - start)):.3f} token/s) \n")
+    return response
+
+while True:
+    user_input = input("You: ").strip()
+    dialogue_history.append({"role": "user", "content": user_input})
+
+    dialogue_history = truncate(dialogue_history)
+    print("Model:")
+    response = chat(dialogue_history)
+
+    model_reply = tokenizer.decode(response[0], skip_special_tokens=True).split("<|assistant|>")[-1].strip()
+    dialogue_history.append({"role": "assistant", "content": model_reply})
